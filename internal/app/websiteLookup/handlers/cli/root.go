6b2daa097d778d31816ea2b45a7735fc10611a63
Vinicius Leme Alves <vibalta@gmail.com>
2021-04-13 18:02:24 -0300

    refactor: uses cobra version property

--- internal/app/websiteLookup/handlers/cli/root.go
+++ internal/app/websiteLookup/handlers/cli/root.go
@@ -1,33 +1,19 @@
 package cli
 
 import (
-	"fmt"
 	"websiteLookup/internal/app/websiteLookup/global"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version bool
 	address string
 	rootCmd = &cobra.Command{
-		Use:   "wlc",
-		Short: "Let's you query IPs, CNAMEs, MX records and Name Servers!",
-		Run:   showVersion,
+		Use:     "wlc",
+		Short:   "Let's you query IPs, CNAMEs, MX records and Name Servers!",
+		Version: global.Version,
 	}
 )
-
-func init() {
-	rootCmd.Flags().BoolVarP(&version, "version", "v", false, "Gets the application version")
-}
-
-func showVersion(cmd *cobra.Command, args []string) {
-	if version {
-		fmt.Printf("Website Lookup CLI v%v\n", global.Version)
-	} else {
-		cmd.Help()
-	}
-}
 
 // Execute will initiate the application
 func Execute() error {
